3ca9ac56e5c9d0ef6ffd9b8467eb9fbe3a824b55
amShubhama <shubham14sk32@gmail.com>
2025-09-14 22:06:30 +0530

    Added filter logics to filter the rooms

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-hot-toast';
 const ListRoom = () => {
 
     const [rooms, setRooms] = useState([]);
-    const { axios, getToken, user } = useAppContext();
+    const { axios, getToken, user, currency } = useAppContext();
 
     //
     const fetchRooms = async () => {
@@ -75,7 +75,7 @@ const ListRoom = () => {
                                         {item.amenities.join(', ')}
                                     </td>
                                     <td className='py-3 px-4 text-gray-700 border-t border-gray-300 '>
-                                        {item.pricePerNight}
+                                        {currency}{item.pricePerNight}
                                     </td>
                                     <td className='py-3 px-4 text-red-500 tet-sm text-center border-t border-gray-300 '>
                                         <label className='relative inline-flex items-center cursor-pointer text-gray-900 gap-3'>
